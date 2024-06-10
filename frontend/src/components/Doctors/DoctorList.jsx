65d62917ebdb5c18c20b8b45345e83648334e47e
Oussamaabdelwahe <109514703+Oussamaabdelwahe@users.noreply.github.com>
2024-06-10 14:49:12 +0200

    finsh

---
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -0,0 +1,18 @@
+
+
+import DoctorCard from './DoctorCard';
+import { BASE_URL } from '../../config';
+import useFetchData from '../../hooks/useFetchData';
+import Loader from '../../components/Loder/Loading'
+const DoctorList = () => {
+  const {data:doctors,loading,error}= useFetchData(`${BASE_URL}/doctors`)
+  return (<>
+  {loading && <Loader/>}
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+        {doctors.map(doctor=>(<DoctorCard key={doctor._id} doctor={doctor}/>))}
+        </div>
+        </>
+  )
+}
+
+export default DoctorList
